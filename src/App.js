4209c08be3eab1cdb4329db14f125839f2730731
sbrycebarker <sbrycesti@gmail.com>
2019-04-12 01:32:05 -0600

    init setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+class Header extends Component {
+  render() {
+    return (
+      <header className="App-header">
+        <h1>The Writers Gang</h1>
+      </header>
+    )
+  }
+}
+
+class Hero extends Component {
+  render() {
+    return (
+      <div id="hero">
+        <div>
+          <h3>Write the Story!</h3>
+        </div>
+        <input placeholder="Email"></input>
+        <button>Submit</button>
+      </div>
+    )
+  }
+}
+
+class Content extends Component {
+  render() {
+    return (
+      <div id="content">
+        <div class="stuff"></div>
+        <div class="stuff"></div>
+        <div class="stuff"></div>
+        <div class="stuff"></div>
+        <div class="stuff"></div>
+        <div class="stuff"></div>
+      </div>
+    )
+  }
+}
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Header />
+        <Hero />
+        <Content />
       </div>
     );
   }
